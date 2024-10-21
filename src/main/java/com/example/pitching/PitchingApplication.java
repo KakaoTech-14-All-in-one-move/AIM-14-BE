831323c563cbc15f36862939dd90d96dbd7b414c
teddy.kim <qazkyj0310@gmail.com>
2024-10-21 16:25:30 +0900

    add: init project with dependencies

---
+++ src/main/java/com/example/pitching/PitchingApplication.java
@@ -0,0 +1,13 @@
+package com.example.pitching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitchingApplication.class, args);
+	}
+
+}
