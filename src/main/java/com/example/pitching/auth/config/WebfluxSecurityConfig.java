8045f4772c98160c950bb776f996d336b24e5142
teddy.kim <qazkyj0310@gmail.com>
2024-10-24 16:42:57 +0900

    AIM-50 add: base spring security

---
+++ src/main/java/com/example/pitching/auth/config/WebfluxSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.pitching.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+public class WebfluxSecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().authenticated()
+                )
+                .httpBasic(withDefaults())
+                .formLogin(withDefaults());
+        return http.build();
+    }
+}
