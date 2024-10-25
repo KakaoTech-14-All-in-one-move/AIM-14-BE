ec64a26ded2bc575f198b13d40718f83aa5b27a3
teddy.kim <qazkyj0310@gmail.com>
2024-10-25 15:47:23 +0900

    AIM-50 add: user repository

---
+++ src/main/java/com/example/pitching/auth/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.pitching.auth.repository;
+
+import com.example.pitching.auth.domain.User;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
+
+public interface UserRepository extends ReactiveCrudRepository<User, Long> {
+    Mono<User> findByUsername(String username);
+}
