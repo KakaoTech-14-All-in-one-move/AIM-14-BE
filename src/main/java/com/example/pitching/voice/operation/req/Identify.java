beaa6cb124064de04244372b8b7a8ec1ff53b72e
neo <maiload77@gmail.com>
2024-10-26 12:23:06 +0900

    fix: Remove Identify data

--- src/main/java/com/example/pitching/voice/operation/req/Identify.java
+++ src/main/java/com/example/pitching/voice/operation/req/Identify.java
@@ -2,13 +2,8 @@ package com.example.pitching.voice.operation.req;
 
 import com.example.pitching.voice.operation.Operation;
 import com.example.pitching.voice.operation.code.ConnectReqOp;
-import com.example.pitching.voice.operation.req.data.IdentifyData;
 
 public record Identify(
-        ConnectReqOp op,
-        IdentifyData data
+        ConnectReqOp op
 ) implements Operation {
-//    public String getToken() {
-//        return this.data.token();
-//    }
 }
--- src/main/java/com/example/pitching/voice/operation/req/data/IdentifyData.java
+++
@@ -1,7 +0,0 @@
-package com.example.pitching.voice.operation.req.data;
-
-public record IdentifyData(
-        String token, // -> Spring Security 로 대체
-        boolean compress
-) {
-}
