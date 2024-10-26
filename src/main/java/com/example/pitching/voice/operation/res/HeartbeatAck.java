f760cd97d37081c6e8fdfc2ffdcff97d1dcccb15
neo <maiload77@gmail.com>
2024-10-26 11:53:50 +0900

    fix: Rename from beat

--- src/main/java/com/example/pitching/voice/operation/res/HeartbeatAck.java
+++ src/main/java/com/example/pitching/voice/operation/res/HeartbeatAck.java
@@ -3,8 +3,8 @@ package com.example.pitching.voice.operation.res;
 import com.example.pitching.voice.operation.Operation;
 import com.example.pitching.voice.operation.code.ConnectResOp;
 
-public record Beat(ConnectResOp op) implements Operation {
-    public static Beat of() {
-        return new Beat(ConnectResOp.HEARTBEAT_ACK);
+public record HeartbeatAck(ConnectResOp op) implements Operation {
+    public static HeartbeatAck of() {
+        return new HeartbeatAck(ConnectResOp.HEARTBEAT_ACK);
     }
 }
