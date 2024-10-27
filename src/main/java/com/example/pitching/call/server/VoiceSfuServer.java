7b07378a0c39d5596f0a705a86e754cac3004376
neo <maiload77@gmail.com>
2024-10-27 18:33:44 +0900

    feat: Add Sample VoiceSfuServer

---
+++ src/main/java/com/example/pitching/call/server/VoiceSfuServer.java
@@ -0,0 +1,51 @@
+package com.example.pitching.call.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.socket.DatagramPacket;
+import io.netty.util.CharsetUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+import reactor.netty.udp.UdpServer;
+
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class VoiceSfuServer {
+    private final ConcurrentHashMap<String, InetSocketAddress> clients = new ConcurrentHashMap<>();
+
+    @EventListener(ApplicationStartedEvent.class)
+    public void start() {
+        UdpServer.create()
+                .wiretap(true)
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
+                .handle((in, out) ->
+                        out.sendObject(
+                                in.receiveObject()
+                                        .cast(DatagramPacket.class)
+                                        .map(packet -> {
+                                            ByteBuf receivedBuf = packet.content().retain();
+                                            log.info("MESSAGE : {}", receivedBuf.toString(CharsetUtil.UTF_8));
+                                            ByteBuf buf = Unpooled.copiedBuffer("hello", CharsetUtil.UTF_8);
+                                            return new DatagramPacket(buf, packet.sender());
+                                        })
+                        )
+                )
+                .bindAddress(() -> new InetSocketAddress(9090))
+                .bindNow(Duration.ofSeconds(30))
+                .onDispose()
+                .doOnSubscribe(s -> log.info("UDP Server successfully bound to port 9090."))
+                .doOnError(e -> log.error("Failed to bind UDP server.", e))
+                .block();
+    }
+}
