c616518899a60ff31a94f2c373cd73a8bc76b60f
neo <maiload77@gmail.com>
2024-10-28 09:30:53 +0900

    fix: Remove channel field

--- src/main/java/com/example/pitching/call/controller/VoiceController.java
+++ src/main/java/com/example/pitching/call/controller/VoiceController.java
@@ -3,38 +3,22 @@ package com.example.pitching.call.controller;
 import com.example.pitching.call.dto.properties.ServerProperties;
 import com.example.pitching.call.dto.response.UrlRes;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class VoiceController {
 
-    // TODO: 메서드가 늘어나면 GlobalExceptionHandler로 분리할 예정
     // TODO: 프론트에서 요청할 필요가 없다고 느껴지면 삭제할 수도 있음
-    private final static List<String> channels = List.of("voice", "video");
     private final ServerProperties serverProperties;
 
-    @GetMapping(value = "/call/{channel}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<UrlRes> getUrl(@PathVariable(value = "channel") String channel) {
-        if (!channels.contains(channel))
-            return Mono.error(new IllegalArgumentException("Invalid channel"));
-        return Mono.just(UrlRes.of(serverProperties.getUrl(channel)));
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Mono<Map<String, String>> handleResponseStatusException(IllegalArgumentException ex) {
-        Map<String, String> errorBody = new HashMap<>();
-        errorBody.put("error", ex.getMessage());
-
-        return Mono.just(errorBody);
+    @GetMapping(value = "/call", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<UrlRes> getConnectUrl() {
+        return Mono.just(UrlRes.of(serverProperties.getUrl()));
     }
 }
--- src/main/java/com/example/pitching/call/dto/properties/ServerProperties.java
+++ src/main/java/com/example/pitching/call/dto/properties/ServerProperties.java
@@ -7,12 +7,12 @@ import java.time.Duration;
 
 @ConfigurationProperties("server")
 public record ServerProperties(int port, Call call) {
-    public String getUrl(String channel) {
-        return this.call.makeUrl(this.port, channel);
+    public String getUrl() {
+        return this.call.makeUrl(this.port);
     }
 
     public String getUrlForTest(int port, String channel) {
-        return this.call.makeUrl(port, channel);
+        return this.call.makeUrl(port);
     }
 
     public long getHeartbeatInterval() {
@@ -29,7 +29,7 @@ public record ServerProperties(int port, Call call) {
 
     public record Call(String protocol, String host, String websocketPath, int version,
                        Duration heartbeatInterval) {
-        private String makeUrl(int port, String channel) {
+        private String makeUrl(int port) {
             return UriComponentsBuilder.newInstance()
                     .scheme(protocol)
                     .host(host)
