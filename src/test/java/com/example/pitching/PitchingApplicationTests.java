1b40f8090ea8e80d1ff1c1da703fcd6c356f9fa7
teddy.kim <qazkyj0310@gmail.com>
2024-10-30 16:59:18 +0900

    AIM-50 add: bcrypt password test

--- src/test/java/com/example/pitching/PitchingApplicationTests.java
+++ src/test/java/com/example/pitching/PitchingApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.pitching;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class PitchingApplicationTests {
@@ -10,4 +11,14 @@ class PitchingApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void generateAndVerifyPassword() {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String rawPassword = "password123";
+		String hashedPassword = encoder.encode(rawPassword);
+		System.out.println("New hashed password: " + hashedPassword);
+
+		boolean matches = encoder.matches(rawPassword, hashedPassword);
+		System.out.println("Verification with new hash: " + matches);
+	}
 }
