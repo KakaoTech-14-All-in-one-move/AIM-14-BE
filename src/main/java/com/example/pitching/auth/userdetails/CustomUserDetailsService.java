8b4bb7cd69aa964e4d08973938ac749df0fd828b
teddy.kim <qazkyj0310@gmail.com>
2024-11-02 16:57:23 +0900

    AIM-50 add: error handling

--- src/main/java/com/example/pitching/auth/userdetails/CustomUserDetailsService.java
+++ src/main/java/com/example/pitching/auth/userdetails/CustomUserDetailsService.java
@@ -2,12 +2,15 @@ package com.example.pitching.auth.userdetails;
 
 import com.example.pitching.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
+@Primary
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements ReactiveUserDetailsService {
     private final UserRepository userRepository;
@@ -15,6 +18,9 @@ public class CustomUserDetailsService implements ReactiveUserDetailsService {
     @Override
     public Mono<UserDetails> findByUsername(String email) {
         return userRepository.findByEmail(email)
+                .switchIfEmpty(Mono.error(
+                        new UsernameNotFoundException("User not found with email: " + email)
+                ))
                 .map(CustomUserDetails::new);
     }
 }
