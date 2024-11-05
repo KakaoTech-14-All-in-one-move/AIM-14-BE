0ebfd8d7438c463d34924d4db2f149affc9a8660
teddy.kim <qazkyj0310@gmail.com>
2024-11-05 13:03:34 +0900

    AIM-67 refactor: exception import

--- src/main/java/com/example/pitching/auth/exception/InvalidCredentialsException.java
+++ src/main/java/com/example/pitching/auth/exception/InvalidCredentialsException.java
@@ -1,5 +1,7 @@
 package com.example.pitching.auth.exception;
 
+import org.springframework.security.core.AuthenticationException;
+
 public class InvalidCredentialsException extends AuthenticationException {
     public InvalidCredentialsException(String message) {
         super(message);
--- src/main/java/com/example/pitching/auth/exception/InvalidTokenException.java
+++ src/main/java/com/example/pitching/auth/exception/InvalidTokenException.java
@@ -1,6 +1,6 @@
 package com.example.pitching.auth.exception;
 
-public class InvalidTokenException extends AuthenticationException {
+public class InvalidTokenException extends RuntimeException {
     public InvalidTokenException(String message) {
         super(message);
     }
