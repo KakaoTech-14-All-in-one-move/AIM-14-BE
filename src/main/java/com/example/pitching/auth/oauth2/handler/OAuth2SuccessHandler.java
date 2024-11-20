1594f8ea27a90fecd07fe16ebf5fb62d9dec0849
teddy.kim <qazkyj0310@gmail.com>
2024-11-20 17:06:41 +0900

    AIM-143 add: server when success oauth2

--- src/main/java/com/example/pitching/auth/oauth2/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/example/pitching/auth/oauth2/handler/OAuth2SuccessHandler.java
@@ -5,6 +5,10 @@ import com.example.pitching.auth.dto.TokenInfo;
 import com.example.pitching.auth.dto.UserInfo;
 import com.example.pitching.auth.repository.UserRepository;
 import com.example.pitching.auth.jwt.JwtTokenProvider;
+import com.example.pitching.user.dto.ServerInfo;
+import com.example.pitching.user.repository.ServerRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,6 +18,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.server.WebFilterExchange;
 import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -21,6 +26,8 @@ import reactor.core.publisher.Mono;
 import reactor.util.function.Tuples;
 
 import java.net.URI;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 @Component
@@ -29,6 +36,7 @@ import java.util.Map;
 public class OAuth2SuccessHandler implements ServerAuthenticationSuccessHandler {
     private final JwtTokenProvider jwtTokenProvider;
     private final UserRepository userRepository;
+    private final ServerRepository serverRepository;
     @Value("${front.url}")
     private String frontURL;
 
@@ -48,34 +56,55 @@ public class OAuth2SuccessHandler implements ServerAuthenticationSuccessHandler 
                                     .then(Mono.just(User.createNewUser(email, name, null, null)));
                         })
                 )
-                .map(user -> {
-                    TokenInfo tokenInfo = jwtTokenProvider.createTokenInfo(email);
-                    UserInfo userInfo = new UserInfo(
-                            user.getEmail(),
-                            user.getUsername(),
-                            user.getProfileImage()
-                    );
-                    return Tuples.of(tokenInfo, userInfo);
-                })
+                .flatMap(user -> serverRepository.findServersByUserEmail(user.getEmail())
+                        .map(server -> new ServerInfo(
+                                server.getServerId(),
+                                server.getServerName(),
+                                server.getServerImage()
+                        ))
+                        .collectList()
+                        .map(servers -> {
+                            TokenInfo tokenInfo = jwtTokenProvider.createTokenInfo(email);
+                            UserInfo userInfo = new UserInfo(
+                                    user.getEmail(),
+                                    user.getUsername(),
+                                    user.getProfileImage(),
+                                    servers
+                            );
+                            return Tuples.of(tokenInfo, userInfo);
+                        }))
                 .flatMap(tuple -> {
                     TokenInfo tokenInfo = tuple.getT1();
                     UserInfo userInfo = tuple.getT2();
 
+                    // servers를 JSON 문자열로 변환
+                    String serversJson = null;
+                    try {
+                        serversJson = new ObjectMapper()
+                                .writeValueAsString(userInfo.servers());
+                    } catch (JsonProcessingException e) {
+                        throw new RuntimeException(e);
+                    }
+
                     String redirectUrl = UriComponentsBuilder
-                            .fromUriString(frontURL+"oauth2/callback")
+                            .fromUriString(frontURL + "oauth2/callback")
                             .queryParam("accessToken", tokenInfo.accessToken())
                             .queryParam("refreshToken", tokenInfo.refreshToken())
                             .queryParam("email", userInfo.email())
                             .queryParam("username", userInfo.username())
                             .queryParam("profile_image", userInfo.profile_image())
+                            .queryParam("servers", URLEncoder.encode(serversJson, StandardCharsets.UTF_8))
                             .build()
-                            .encode()
                             .toUriString();
 
                     exchange.getResponse().setStatusCode(HttpStatus.TEMPORARY_REDIRECT);
                     exchange.getResponse().getHeaders().setLocation(URI.create(redirectUrl));
 
                     return exchange.getResponse().setComplete();
+                })
+                .onErrorResume(JsonProcessingException.class, e -> {
+                    log.error("Error processing servers to JSON", e);
+                    return Mono.error(new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR));
                 });
     }
 
