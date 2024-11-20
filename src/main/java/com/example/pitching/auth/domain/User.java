bfccdce2986e75e7f5f7768cdca3c1900bd9cd3a
teddy.kim <qazkyj0310@gmail.com>
2024-11-20 17:05:08 +0900

    AIM-143 add: server in user

--- src/main/java/com/example/pitching/auth/domain/User.java
+++ src/main/java/com/example/pitching/auth/domain/User.java
@@ -1,10 +1,15 @@
 package com.example.pitching.auth.domain;
 
+import com.example.pitching.user.domain.Server;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.PersistenceCreator;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Table("users")
 @Getter
@@ -21,8 +26,11 @@ public class User {
     private String password;
     private String role;
 
+    @Transient  // DB에 매핑되지 않음을 표시
+    private List<Server> servers = new ArrayList<>();
+
     public static User createNewUser(String email, String username,
                                      String profileImage, String password) {
-        return new User(email, username, profileImage, password, "USER");
+        return new User(email, username, profileImage, password, "USER", new ArrayList<>());
     }
 }
