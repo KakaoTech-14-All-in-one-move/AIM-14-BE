7499592e52bea52cd92b666413cf6483d6dab83b
teddy.kim <qazkyj0310@gmail.com>
2024-11-20 17:05:26 +0900

    AIM-143 add: server in user info

--- src/main/java/com/example/pitching/auth/dto/UserInfo.java
+++ src/main/java/com/example/pitching/auth/dto/UserInfo.java
@@ -1,7 +1,12 @@
 package com.example.pitching.auth.dto;
 
+import com.example.pitching.user.dto.ServerInfo;
+
+import java.util.List;
+
 public record UserInfo(
         String email,
         String username,
-        String profile_image
+        String profile_image,
+        List<ServerInfo> servers
 ) {}
