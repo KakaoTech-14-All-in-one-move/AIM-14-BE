43fee2ad1f95604059c1c98e86b3f1c9a600acc2
teddy.kim <qazkyj0310@gmail.com>
2024-11-20 17:11:17 +0900

    AIM-143 add: server repository

---
+++ src/main/java/com/example/pitching/user/repository/ServerRepository.java
@@ -0,0 +1,23 @@
+package com.example.pitching.user.repository;
+
+import com.example.pitching.user.domain.Server;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Repository
+public interface ServerRepository extends ReactiveCrudRepository<Server, Long> {
+    Mono<Server> findByServerId(Long serverId);
+
+    @Query("""
+        SELECT s.* FROM servers s 
+        INNER JOIN user_server_memberships usm ON s.server_id = usm.server_id 
+        WHERE usm.email = :email
+        """)
+    Flux<Server> findServersByUserEmail(String email);
+
+    @Query("UPDATE servers SET server_name = :serverName, server_image = :serverImage WHERE server_id = :serverId")
+    Mono<Void> updateServerInfo(Long serverId, String serverName, String serverImage);
+}
