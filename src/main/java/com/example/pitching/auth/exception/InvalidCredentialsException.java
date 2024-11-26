78c6477fae1e8b05578e0e25ffc51711cbe34fa3
teddy.kim <qazkyj0310@gmail.com>
2024-11-26 16:31:08 +0900

    AIM-149 remove: unnecessary exception object

--- src/main/java/com/example/pitching/auth/exception/InvalidCredentialsException.java
+++
@@ -1,9 +0,0 @@
-package com.example.pitching.auth.exception;
-
-import org.springframework.security.core.AuthenticationException;
-
-public class InvalidCredentialsException extends AuthenticationException {
-    public InvalidCredentialsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/pitching/auth/exception/TokenExpiredException.java
+++
@@ -1,9 +0,0 @@
-package com.example.pitching.auth.exception;
-
-import org.springframework.security.core.AuthenticationException;
-
-public class TokenExpiredException extends AuthenticationException {
-    public TokenExpiredException(String message) {
-        super(message);
-    }
-}
