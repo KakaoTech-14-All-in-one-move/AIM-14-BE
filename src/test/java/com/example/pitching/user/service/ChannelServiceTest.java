1ff6127e2fb561b8e0d957a67c72b356b2133ba7
teddy.kim <qazkyj0310@gmail.com>
2024-12-05 15:31:22 +0900

    AIM-170 add: active profile test

--- src/test/java/com/example/pitching/user/service/ChannelServiceTest.java
+++ src/test/java/com/example/pitching/user/service/ChannelServiceTest.java
@@ -14,6 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -24,6 +25,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
+@ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
 class ChannelServiceTest {
     @Mock
