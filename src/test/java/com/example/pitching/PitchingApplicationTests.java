5af34ccfbbbf58280a0ac45bf652e8a8ecf974d1
teddy.kim <qazkyj0310@gmail.com>
2024-12-05 10:52:18 +0900

    AIM-158 remove: unnecessary import

--- src/test/java/com/example/pitching/PitchingApplicationTests.java
+++ src/test/java/com/example/pitching/PitchingApplicationTests.java
@@ -1,8 +1,6 @@
 package com.example.pitching;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class PitchingApplicationTests {
