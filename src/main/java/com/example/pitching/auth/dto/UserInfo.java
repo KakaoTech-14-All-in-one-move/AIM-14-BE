53da99a97e6e8ab2d385dce230d35d487134fc5b
teddy.kim <qazkyj0310@gmail.com>
2024-12-08 14:28:55 +0900

    AIM-178 add: user id to the user info

--- src/main/java/com/example/pitching/auth/dto/UserInfo.java
+++ src/main/java/com/example/pitching/auth/dto/UserInfo.java
@@ -7,6 +7,7 @@ import java.util.List;
 public record UserInfo(
         String email,
         String username,
+        Long userId,
         String profile_image,
         List<ServerInfo> servers
 ) {}
