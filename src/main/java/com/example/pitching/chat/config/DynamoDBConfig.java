6b1bab4d38f6dc677e463bf066cb366340c0b1d3
teddy.kim <qazkyj0310@gmail.com>
2024-12-09 16:30:10 +0900

    AIM-176 add: dynamo db config setting

--- src/main/java/com/example/pitching/chat/config/DynamoDBConfig.java
+++ src/main/java/com/example/pitching/chat/config/DynamoDBConfig.java
@@ -1,10 +1,14 @@
 package com.example.pitching.chat.config;
 
+import com.example.pitching.chat.domain.ChatMessage;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbAsyncTable;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
@@ -37,5 +41,17 @@ public class DynamoDBConfig {
                 .credentialsProvider(StaticCredentialsProvider.create(
                         AwsBasicCredentials.create(accessKey, secretKey)))
                 .build();
+    }
+
+    @Bean
+    public DynamoDbEnhancedAsyncClient dynamoDbEnhancedAsyncClient(DynamoDbAsyncClient dynamoDbAsyncClient) {
+        return DynamoDbEnhancedAsyncClient.builder()
+                .dynamoDbClient(dynamoDbAsyncClient)
+                .build();
+    }
+
+    @Bean
+    public DynamoDbAsyncTable<ChatMessage> chatMessageTable(DynamoDbEnhancedAsyncClient enhancedAsyncClient) {
+        return enhancedAsyncClient.table("ChatMessages", TableSchema.fromBean(ChatMessage.class));
     }
 }
