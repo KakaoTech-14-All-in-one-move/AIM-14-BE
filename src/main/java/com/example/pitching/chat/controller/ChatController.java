6b51b65037623d3a9c3a2d215fbeefc154d91e7c
teddy.kim <qazkyj0310@gmail.com>
2024-12-16 17:09:55 +0900

    AIM-176 add: websocket dependency

--- src/main/java/com/example/pitching/chat/controller/ChatController.java
+++ src/main/java/com/example/pitching/chat/controller/ChatController.java
@@ -1,42 +1,28 @@
 package com.example.pitching.chat.controller;
 
 import com.example.pitching.chat.domain.ChatMessage;
-import com.example.pitching.chat.dto.MessageRequest;
 import com.example.pitching.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/chat")
 public class ChatController {
-    private final SimpMessageSendingOperations messagingTemplate;
     private final ChatService chatService;
 
-    @MessageMapping("/chat/{channelId}")
-    public Mono<Void> message(@DestinationVariable String channelId, MessageRequest request) {
-        return chatService.saveMessage(
-                        channelId,
-                        ChatMessage.MessageType.TALK,
-                        request.sender(),
-                        request.content(),
-                        request.profileImage()
-                )
-                .doOnSuccess(savedMessage ->
-                        messagingTemplate.convertAndSend("/sub/chat/" + channelId, savedMessage))
-                .then();
+    @GetMapping("/channels/{channelId}/messages")
+    public Flux<ChatMessage> getChannelMessages(@PathVariable Long channelId) {
+        return chatService.getChannelMessages(channelId);
     }
 
-    @GetMapping("/channels/{channelId}/messages")
-    public Flux<ChatMessage> getChannelMessages(@PathVariable String channelId) {
-        return chatService.getChannelMessages(channelId);
+    @DeleteMapping("/channels/{channelId}/messages")
+    public Mono<ResponseEntity<Void>> deleteChannelMessages(@PathVariable Long channelId) {
+        return chatService.deleteChannelMessages(channelId)
+                .then(Mono.just(ResponseEntity.ok().<Void>build()))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 }
