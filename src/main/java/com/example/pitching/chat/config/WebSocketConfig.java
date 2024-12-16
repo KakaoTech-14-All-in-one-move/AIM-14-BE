91724ed5e174929c1916c7540b543f800a47fa51
teddy.kim <qazkyj0310@gmail.com>
2024-12-16 17:10:38 +0900

    AIM-176 refactor: change uri

--- src/main/java/com/example/pitching/chat/config/WebSocketConfig.java
+++ src/main/java/com/example/pitching/chat/config/WebSocketConfig.java
@@ -7,15 +7,17 @@ import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
 import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class WebSocketConfig {
+
     @Bean
     public HandlerMapping webSocketHandlerMapping(ChatWebSocketHandler chatWebSocketHandler) {
         Map<String, WebSocketHandler> map = new HashMap<>();
-        map.put("/ws-stomp", chatWebSocketHandler);
+        map.put("/ws/chat/{channelId}", chatWebSocketHandler);
 
         SimpleUrlHandlerMapping handlerMapping = new SimpleUrlHandlerMapping();
         handlerMapping.setOrder(-1);
