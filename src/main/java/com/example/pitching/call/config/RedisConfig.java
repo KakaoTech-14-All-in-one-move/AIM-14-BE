392b99e6487c648d4e5996a9ca63e52eb46be044
neo <maiload77@gmail.com>
2024-12-21 14:15:25 +0900

    fix: Fix RedisConfig connection

--- src/main/java/com/example/pitching/call/config/RedisConfig.java
+++ src/main/java/com/example/pitching/call/config/RedisConfig.java
@@ -8,6 +8,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
 
 @Slf4j
 @Configuration
@@ -29,6 +32,22 @@ public class RedisConfig {
     @Bean
     public RedisReactiveCommands<String, String> redisCommands(StatefulRedisConnection<String, String> connection) {
         return connection.reactive();
+    }
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory(RedisProperties redisProperties) {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisProperties.host());
+        config.setPort(redisProperties.port());
+
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(config);
+        factory.afterPropertiesSet();
+        return factory;
+    }
+
+    @Bean
+    public ReactiveStringRedisTemplate reactiveStringRedisTemplate(LettuceConnectionFactory connectionFactory) {
+        return new ReactiveStringRedisTemplate(connectionFactory);
     }
 
     @ConfigurationProperties("redis")
